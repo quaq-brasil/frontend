9a2a5808e31945a98910e6bc1154eae30d7260de
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2023-01-06 18:59:40 -0300

    initial config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const nextTranslate = require('next-translate');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  ...nextTranslate(),
+  images: {
+    domains: [
+      'upload-quaq.s3.sa-east-1.amazonaws.com',
+      'upload-quaq.s3.amazonaws.com'
+    ]
+  }
 }
 
 module.exports = nextConfig
