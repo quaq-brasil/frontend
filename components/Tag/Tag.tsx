c02d97fc5b9294cb181f64ae3755de03f201de87
felpst <felipe.t.v.d@gmail.com>
2023-01-09 16:59:29 -0400

    fix(component): tag

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -47,46 +47,46 @@ export const Tag = (props: TagProps) => {
     <div className={`bg-white ${props.variant !== 'icn-txt-xl' ? 'inline-block' : ''} rounded-full`}>
       <>
       {props.variant === "txt" && 
-        <p className='flex row justify-center content-center items-center gap-[10px] h-[40px] px-[10px] lg:h-[52px] lg:px-3 lg:text-[20px]'>
+        <p className='flex row justify-center content-center items-center gap-[0.625rem] h-[2.5rem] px-[0.625rem] lg:h-[3.25rem] lg:px-3 lg:text-[1.25rem]'>
           {props.text}
         </p>
       }
       {props.variant === "txt-img" && 
-        <div className='flex row justify-center content-center items-center gap-[8px] h-[40px] pr-[2px] pl-[10px] lg:h-[52px] lg:pr-[3px] lg:pl-[12px] lg:gap-[12px]'>
-          <p className='lg:text-[20px]'>{props.text}</p>
-          <Image className='h-[36px] w-[36px] rounded-full lg:h-[46px] lg:w-[46px]' src={props.img_url} width={100} height={100} alt=""/>
+        <div className='flex row justify-center content-center items-center gap-[0.5rem] h-[2.5rem] pr-[0.125rem] pl-[0.625rem] lg:h-[3.25rem] lg:pr-[0.1875rem] lg:pl-[0.75rem] lg:gap-[0.75rem]'>
+          <p className='lg:text-[1.25rem]'>{props.text}</p>
+          <Image className='h-[2.25rem] w-[2.25rem] rounded-full lg:h-[2.875rem] lg:w-[2.875rem]' src={props.img_url} width={100} height={100} alt=""/>
         </div>
       }
       {props.variant === "img-txt" && 
-        <div className='flex row justify-center content-center items-center gap-[8px] h-[40px] pr-[10px] pl-[2px] lg:h-[52px] lg:pr-[12px] lg:pl-[3px] lg:gap-[12px]'>
-          <Image className='h-[36px] w-[36px] rounded-full lg:h-[46px] lg:w-[46px]' src={props.img_url} width={100} height={100} alt=""/>
+        <div className='flex row justify-center content-center items-center gap-[0.5rem] h-[2.5rem] pr-[0.625rem] pl-[0.125rem] lg:h-[3.25rem] lg:pr-[0.75rem] lg:pl-[0.1875rem] lg:gap-[0.75rem]'>
+          <Image className='h-[2.25rem] w-[2.25rem] rounded-full lg:h-[2.875rem] lg:w-[2.875rem]' src={props.img_url} width={100} height={100} alt=""/>
           <p className='lg:text-[20px]'>{props.text}</p>
         </div>
       }
       {props.variant === "img" && 
-        <Image className='h-[36px] w-[36px] rounded-full m-[2px] lg:h-[46px] lg:w-[46px] lg:m-[3px]' src={props.img_url} width={100} height={100} alt=""/>
+        <Image className='h-[2.25rem] w-[2.25rem] rounded-full m-[0.125rem] lg:h-[2.875rem] lg:w-[2.875rem] lg:m-[0.1875rem]' src={props.img_url} width={100} height={100} alt=""/>
       }
       {props.variant === "txt-icn" && 
-        <div className='flex row gap-[10px] content-center justify-center items-center h-[40px] px-[10px] lg:text-[20px] lg:px-3 lg:h-[52px]'>
-          <p className='lg:text-[20px]'>{props.text}</p>
-          <><props.icon className='w-[20px] h-[20px] lg:w-[25px] lg:h-[25px]'/></>
+        <div className='flex row gap-[0.625rem] content-center justify-center items-center h-[2.5rem] px-[0.625rem] lg:text-[1.25rem] lg:px-3 lg:h-[3.25rem]'>
+          <p className='lg:text-[1.25rem]'>{props.text}</p>
+          <><props.icon className='w-[1.25rem] h-[1.25rem] lg:w-[1.5625rem] lg:h-[1.5625rem]'/></>
         </div>
       }
       {props.variant === "icn-txt" && 
-        <div className='flex row gap-[10px] content-center justify-center items-center h-[40px] px-[10px] lg:text-[20px] lg:px-3 lg:h-[52px]'>
-          <><props.icon className='w-[20px] h-[20px] lg:w-[25px] lg:h-[25px]' /></>
-          <p className='lg:text-[20px]'>{props.text}</p>
+        <div className='flex row gap-[0.625rem] content-center justify-center items-center h-[2.5rem] px-[0.625rem] lg:text-[1.25rem] lg:px-3 lg:h-[3.25rem]'>
+          <><props.icon className='w-[1.25rem] h-[1.25rem] lg:w-[1.5625rem] lg:h-[1.5625rem]' /></>
+          <p className='lg:text-[1.25rem]'>{props.text}</p>
         </div>
       }
       {props.variant === "icn-txt-xl" && 
-        <div className='flex row gap-[10px] content-center justify-start items-center h-[40px] px-[10px] lg:text-[20px] lg:px-3 lg:h-[52px]'>
-          <><props.icon className='w-[20px] h-[20px] lg:w-[25px] lg:h-[25px' /></>
-          <p className='lg:text-[20px]'>{props.text}</p>
+        <div className='flex row gap-[0.625rem] content-center justify-start items-center h-[2.5rem] px-[0.625rem] lg:text-[1.25rem] lg:px-3 lg:h-[3.25rem]'>
+          <><props.icon className='w-[1.25rem] h-[1.25rem] lg:w-[1.5625rem] lg:h-[1.5625rem]' /></>
+          <p className='lg:text-[1.25rem]'>{props.text}</p>
         </div>
       }
       {props.variant === "icn" && 
-        <div className='h-[46px] w-[46px] flex before:content-center justify-center items-center lg:h-[52px] lg:w-[52px]'>
-          {<div className=''><props.icon className='w-[20px] h-[20px] lg:w-[25px] lg:h-[25px' /></div>}
+        <div className='h-[2.875rem] w-[2.875rem] flex before:content-center justify-center items-center lg:h-[3.25rem] lg:w-[3.25rem]'>
+          {<div className=''><props.icon className='w-[1.25rem] h-[1.25rem] lg:w-[1.5625rem] lg:h-[1.5625rem]' /></div>}
         </div>
       }
       </>
