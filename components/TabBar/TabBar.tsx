87ee0e6f4d49d2c149b3043756516dcff85f3618
felpst <felipe.t.v.d@gmail.com>
2023-01-10 18:14:51 -0400

    fix(component): TabBar

--- components/TabBar/TabBar.tsx
+++ components/TabBar/TabBar.tsx
@@ -1,15 +1,15 @@
 import { Tag, TagProps } from "../Tag/Tag"
 
-
-
 type TabBarProps = {
   tags: TagProps[]
 }
 
 export const TabBar = (props: TabBarProps) => {
   return (
-    <div className="flex justify-center w-full h-[74px] bg-gradient-to-t from-slate-200 lg:h-full lg:bg-gradient-to-r lg:w-[236px] lg:py-[43px] lg:pl-[34px] lg:justify-start">
-      <div className="flex flex-row items-center gap-2 overflow-x-auto px-[16px] lg:flex-col lg:items-start lg:gap-3 lg:px-0"> 
+    <div className="flex justify-center w-full h-[74px] bg-gradient-to-t from-slate-200 
+    lg:h-full lg:bg-gradient-to-r lg:w-[236px] lg:py-[43px] lg:pl-[34px] lg:justify-start">
+      <div className="flex flex-row items-center gap-2 overflow-x-auto px-[16px] lg:flex-col 
+      lg:items-start lg:gap-3 lg:px-0"> 
         {props.tags.map((tag, index) => (
           <Tag key={index} {...tag} />
         ))}
