21ff4be7953e3f9cb78e8771e042b0d76542fdba
felpst <felipe.t.v.d@gmail.com>
2023-01-11 21:32:36 -0400

    refactor: index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
-
 import { Example } from "../components/Example/Example";
 
 export default function Home() {
   return (
     <div className="flex justify-center py-5 bg-slate-900">
-      
+      <Example />
     </div>
   );
 }
