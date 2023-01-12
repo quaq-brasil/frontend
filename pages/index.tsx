3a095b76f3012981173f7b6cca9540fefd07049a
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2023-01-12 16:35:29 -0300

    base index

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { Example } from "../components/Example/Example";
 
 export default function Home() {
   return (
-    <div className="flex justify-center py-5 bg-slate-900">
+    <div className="flex justify-center h-screen py-5 bg-slate-500">
       <Example />
     </div>
   );
