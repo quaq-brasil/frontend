480b7e29a659778494ab628fea5a13fed328822f
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2023-01-12 17:09:10 -0300

    fix(component): dialog

--- components/Dialog/DIalog.tsx
+++ components/Dialog/DIalog.tsx
@@ -49,10 +49,10 @@ export function Dialog({
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <HeadlessDialog.Panel
-                className={`fixed transform overflow-y-auto overflow-x-hidden md:w-full max-w-4xl bottom-0
+                className={`fixed transform overflow-y-auto overflow-x-hidden md:w-full max-w-4xl bottom-0 scrollbar-hide
                ${heightClasses[height]} `}
               >
-                <div className="bg-slate-100 overflow-y-auto rounded-t-3xl text-left md:border-2 border-solid border-slate-300 h-full unset-0 pb-20">
+                <div className="bg-slate-100 overflow-y-auto rounded-t-3xl text-left md:border-2 border-solid border-slate-300 h-[calc(100%-0.75rem)] unset-0 pb-20 mt-3">
                   <div className="bg-white w-full h-8 flex items-center justify-center py-2 mb-3">
                     <h4>{title}</h4>
                   </div>
