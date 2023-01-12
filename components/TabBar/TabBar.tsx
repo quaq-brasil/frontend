51dc4d0af3d75bc4ffc64355844754d4f317aa3c
felpst <felipe.t.v.d@gmail.com>
2023-01-12 15:23:19 -0400

    fix(component): TabBar

--- components/TabBar/TabBar.tsx
+++ components/TabBar/TabBar.tsx
@@ -1,19 +1,28 @@
-import { Tag, TagProps } from "../Tag/Tag"
+import { Tag, TagProps } from "../Tag/Tag";
 
 type TabBarProps = {
-  tags: TagProps[]
-}
+  tags: TagProps[];
+  isHidden?: boolean;
+};
 
 export const TabBar = (props: TabBarProps) => {
   return (
-    <div className="flex justify-center w-full h-[74px] bg-gradient-to-t from-slate-200 
-    lg:h-full lg:bg-gradient-to-r lg:w-[236px] lg:py-[43px] lg:pl-[34px] lg:justify-start">
-      <div className="flex flex-row items-center gap-2 overflow-x-auto px-[16px] lg:flex-col 
-      lg:items-start lg:gap-3 lg:px-0"> 
+    <div
+      className={`flex justify-center w-full h-[74px] bg-gradient-to-t from-slate-200 fixed bottom-0 z-20 md:left-0 ${
+        props.isHidden ? "md:hidden" : ""
+      }
+    lg:h-full lg:bg-gradient-to-r lg:w-[236px] lg:py-[43px] lg:pl-[34px] lg:justify-start`}
+    >
+      <div
+        className={`flex flex-row scrollbar-hide items-center gap-2 ${
+          props.tags.length === 1 ? "justify-end" : "justify-between"
+        } overflow-x-auto px-[16px] lg:flex-col 
+      lg:items-start lg:gap-3 lg:px-0 w-full`}
+      >
         {props.tags.map((tag, index) => (
           <Tag key={index} {...tag} />
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
