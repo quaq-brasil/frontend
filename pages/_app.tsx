395fd23d95804f716b61346dad8b975343e76111
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2023-01-13 12:54:31 -0300

    feat: add outfit font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,10 @@ import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import "../styles/global.css";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
