a4f5750c6acc1097a920d025e857585c5cc59180
felpst <felipe.t.v.d@gmail.com>
2023-01-16 16:49:27 -0400

    feat(component): WorkspaceSetup

---
+++ compositions/Onboarding/WorkspaceSetup/WorkspaceSetup.tsx
@@ -0,0 +1,48 @@
+import useTranslation from "next-translate/useTranslation";
+import dynamic from "next/dynamic";
+import { Header } from "../../../components/Header/Header";
+import { TabBar } from "../../../components/TabBar/TabBar";
+import { Tag } from "../../../components/Tag/Tag";
+import { WorkspaceSetupContent } from "./WorkspaceSetupContent";
+
+type WorkspaceSetupProps = {
+  headerImageUrl: string;
+};
+
+const ContextMenu = dynamic(
+  () => import("../../../components/ContextMenu/ContextMenu"),
+  {
+    ssr: false,
+  }
+);
+
+export default function WorkspaceSetup(props: WorkspaceSetupProps) {
+  const text = useTranslation().t;
+
+  function handleTabBar() {
+    return [
+      <Tag
+        key={1}
+        variant="txt"
+        text={text("wssetup:tab1")}
+        onClick={() => console.log("tab1")}
+      />,
+      <Tag
+        key={2}
+        variant="txt"
+        text={text("wssetup:tab2")}
+        onClick={() => console.log("tab2")}
+      />,
+    ];
+  }
+
+  return (
+    <div className="bg-slate-100 fixed inset-0">
+      <Header background_url={props.headerImageUrl}>
+        <Tag variant="txt" text={text("wssetup:titletag")} />
+      </Header>
+      <WorkspaceSetupContent />
+      <TabBar isHidden={false} tags={handleTabBar()} />
+    </div>
+  );
+}
---
+++ compositions/Onboarding/WorkspaceSetup/WorkspaceSetupContent.tsx
@@ -0,0 +1,58 @@
+import useTranslation from "next-translate/useTranslation";
+import dynamic from "next/dynamic";
+import { Button } from "../../../components/Button/Button";
+import { Card } from "../../../components/Card/Card";
+import { CardImageInput } from "../../../components/Card/CardContentVariants/CardImageInput";
+import { CardText } from "../../../components/Card/CardContentVariants/CardText";
+import { CardTextInput } from "../../../components/Card/CardContentVariants/CardTextInput";
+import { ImageSelector } from "../../../components/ImageSelector/ImageSelector";
+
+const QuickIn = dynamic(() => import("../../../components/QuickIn/QuickIn"), {
+  ssr: false,
+});
+
+export function WorkspaceSetupContent() {
+  const text = useTranslation().t;
+
+  function handleFinishSignUp() {}
+
+  return (
+    <div className="w-full h-screen bg-slate-100">
+      <div
+        className="fixed z-20 bottom-0 left-0 right-0 top-[76px] max-w-[1024px] mx-auto
+      bg-slate-100 rounded-t-[25px] overflow-y-scroll scrollbar-hide pt-2 px-2
+      md:pt-4 md:px-4 lg:z-0 lg:rounded-none lg:top-[148px] lg:pt-[1.5rem]"
+      >
+        <div className="flex flex-col gap-2 md:gap-4 items-center">
+          <Card>
+            <CardText label={text("wssetup:firstmessage")} />
+          </Card>
+          <Card>
+            <CardText label={text("wssetup:getwsname")} />
+            <CardTextInput
+              input={{
+                label: text("wssetup:inputwsname"),
+                onChange: (e) => console.log(e),
+                type: "text",
+              }}
+            />
+          </Card>
+          <Card>
+            <CardText label={text("wssetup:uploadimg")} />
+            <CardImageInput
+              imageSelector={
+                <ImageSelector onImageChange={(e) => console.log(e)} />
+              }
+            />
+          </Card>
+          <Button
+            color="slate-900"
+            onClick={handleFinishSignUp}
+            text={text("wssetup:confirm")}
+          />
+          <span className="w-full h-[4rem]"></span>
+        </div>
+      </div>
+    </div>
+  );
+}
