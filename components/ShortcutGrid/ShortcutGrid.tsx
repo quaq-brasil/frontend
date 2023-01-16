f3c036e3a2ab2164e344e68ae8fa9cdabf4748de
felpst <felipe.t.v.d@gmail.com>
2023-01-16 12:57:16 -0400

    fix(component): Shortcut

--- components/ShortcutGrid/ShortcutGrid.tsx
+++ components/ShortcutGrid/ShortcutGrid.tsx
@@ -14,14 +14,14 @@ export const ShortcutGrid = ({ children, onDrag }: ShortcutGridProps) => {
   });
 
   return (
-    <ul
+    <div
       className="grid grid-cols-2 gap-y-2 gap-x-[0.625rem] mb-20
-      lg:gap-y-10 lg:gap-x-0 md:mt-8 lg:grid-cols-3 max-w-[900px] mx-auto"
+      lg:gap-y-10 lg:gap-x-10 md:mt-12 lg:grid-cols-3 max-w-[900px] mx-auto place-items-center"
     >
       {/* <DndContext onDragEnd={onDrag}>
         <SortableContext items={items}>{children}</SortableContext>
       </DndContext> */}
       {children}
-    </ul>
+    </div>
   );
 };
