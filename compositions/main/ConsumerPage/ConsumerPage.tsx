56e04a523e9981f9c6fa45465396f5acd3a14586
felpst <felipe.t.v.d@gmail.com>
2023-01-16 13:41:55 -0400

    feat(component): ConsumerPage

--- compositions/main/ConsumerPage/ConsumerPage.tsx
+++ compositions/main/ConsumerPage/ConsumerPage.tsx
@@ -1,6 +1,7 @@
 import { Menu } from "@headlessui/react";
 import useTranslation from "next-translate/useTranslation";
 import dynamic from "next/dynamic";
+import { UserCirclePlus } from "phosphor-react";
 import { Header } from "../../../components/Header/Header";
 import { TabBar } from "../../../components/TabBar/TabBar";
 import { Tag } from "../../../components/Tag/Tag";
@@ -19,54 +20,88 @@ const ContextMenu = dynamic(
 
 export default function ConsumerPage(props: ConsumerPageProps) {
   const text = useTranslation().t;
+
+  function handleContextMenu() {
+    const isSignedIn = false;
+    if (isSignedIn) {
+      return (
+        <ContextMenu
+          Opener={
+            <Tag
+              variant="img"
+              img_url="https://source.unsplash.com/featured/"
+            />
+          }
+        >
+          <div className="flex flex-col gap-3">
+            <Menu.Item>
+              <Tag variant="txt" text={text("example")} />
+            </Menu.Item>
+            <Menu.Item>
+              <Tag variant="txt" text={text("example")} />
+            </Menu.Item>
+            <Menu.Item>
+              <Tag variant="txt" text={text("example")} />
+            </Menu.Item>
+            <Menu.Item>
+              <Tag variant="txt" text={text("example")} />
+            </Menu.Item>
+          </div>
+        </ContextMenu>
+      );
+    } else {
+      return (
+        <ContextMenu Opener={<Tag variant="icn" icon={UserCirclePlus} />}>
+          <div className="flex flex-col gap-3">
+            <Menu.Item>
+              <Tag variant="txt" text={text("consumerpage:signup")} />
+            </Menu.Item>
+            <Menu.Item>
+              <Tag variant="txt" text={text("consumerpage:login")} />
+            </Menu.Item>
+          </div>
+        </ContextMenu>
+      );
+    }
+  }
+
+  function handleTabBar() {
+    return [
+      <Tag
+        key={1}
+        variant="txt"
+        text={text("consumerpage:explore")}
+        onClick={() => console.log("explore")}
+      />,
+      <Tag
+        key={2}
+        variant="txt"
+        text={text("consumerpage:createpage")}
+        onClick={() => console.log("createpage")}
+      />,
+    ];
+  }
+
+  function handleMainTag() {
+    return (
+      <Tag
+        variant="img-txt"
+        text="example"
+        img_url="https://source.unsplash.com/featured/"
+      />
+    );
+  }
+
   return (
     <>
       <Header
-        reightContent={
-          <ContextMenu
-            Opener={
-              <Tag
-                variant="img"
-                img_url="https://source.unsplash.com/featured/"
-              />
-            }
-          >
-            <div className="flex flex-col gap-3">
-              <Menu.Item>
-                <Tag variant="txt" text={text("consumerpage:titletag")} />
-              </Menu.Item>
-              <Menu.Item>
-                <Tag variant="txt" text={text("consumerpage:titletag")} />
-              </Menu.Item>
-              <Menu.Item>
-                <Tag variant="txt" text={text("consumerpage:titletag")} />
-              </Menu.Item>
-              <Menu.Item>
-                <Tag variant="txt" text={text("consumerpage:titletag")} />
-              </Menu.Item>
-            </div>
-          </ContextMenu>
-        }
+        reightContent={handleContextMenu()}
         background_url={props.headerImageUrl}
       >
-        <Tag variant="txt" text={text("consumerpage:titletag")} />
+        {handleMainTag()}
       </Header>
       <ConsumerPageContent />
-      <TabBar
-        isHidden={false}
-        tags={[
-          {
-            variant: "txt",
-            text: text("consumerpage:tab1a"),
-            onClick: () => console.log("tab1"),
-          },
-          {
-            variant: "txt",
-            text: text("consumerpage:tab2"),
-            onClick: () => console.log("tab2"),
-          },
-        ]}
-      />
+      <TabBar isHidden={false} tags={handleTabBar()} />
     </>
   );
 }
