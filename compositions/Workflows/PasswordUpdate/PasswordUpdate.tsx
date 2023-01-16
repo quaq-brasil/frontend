4cad4190e3001c66931d57516eaa761589ecb5a8
felpst <felipe.t.v.d@gmail.com>
2023-01-16 16:50:03 -0400

    feat(component): PasswordUpdate

---
+++ compositions/Workflows/PasswordUpdate/PasswordUpdate.tsx
@@ -0,0 +1,47 @@
+import useTranslation from "next-translate/useTranslation";
+import dynamic from "next/dynamic";
+import { Header } from "../../../components/Header/Header";
+import { TabBar } from "../../../components/TabBar/TabBar";
+import { Tag } from "../../../components/Tag/Tag";
+import { PasswordUpdateContent } from "./PasswordUpdateContent";
+
+type PasswordUpdateProps = {
+  headerImageUrl: string;
+};
+
+const ContextMenu = dynamic(
+  () => import("../../../components/ContextMenu/ContextMenu"),
+  {
+    ssr: false,
+  }
+);
+
+export default function PasswordUpdate(props: PasswordUpdateProps) {
+  const text = useTranslation().t;
+
+  function handleTabBar() {
+    return [
+      <Tag
+        key={1}
+        variant="txt"
+        text={text("pwupdate:tab1")}
+        onClick={() => console.log("tab1")}
+      />,
+    ];
+  }
+
+  return (
+    <div className="bg-slate-100 fixed inset-0">
+      <Header background_url={props.headerImageUrl}>
+        <Tag
+          variant="img-txt"
+          text="username"
+          img_url="https://source.unsplash.com/featured/"
+        />
+        <Tag variant="txt" text={text("pwupdate:titletag")} />
+      </Header>
+      <PasswordUpdateContent />
+      <TabBar isHidden={false} tags={handleTabBar()} />
+    </div>
+  );
+}
