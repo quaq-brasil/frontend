666fbf1ed06e78c11345c7b1df12dfdd5c305504
felpst <felipe.t.v.d@gmail.com>
2023-01-16 15:03:01 -0400

    feat(component): CreatorPage

---
+++ compositions/main/CreatorPage/CreatorPage.tsx
@@ -0,0 +1,94 @@
+import { Menu } from "@headlessui/react";
+import useTranslation from "next-translate/useTranslation";
+import dynamic from "next/dynamic";
+import { ArrowsCounterClockwise } from "phosphor-react";
+import { Header } from "../../../components/Header/Header";
+import { TabBar } from "../../../components/TabBar/TabBar";
+import { Tag } from "../../../components/Tag/Tag";
+import { CreatorPageContent } from "./CreatorPageContent";
+
+type CreatorPageProps = {
+  headerImageUrl: string;
+};
+
+const ContextMenu = dynamic(
+  () => import("../../../components/ContextMenu/ContextMenu"),
+  {
+    ssr: false,
+  }
+);
+
+export default function CreatorPage(props: CreatorPageProps) {
+  const text = useTranslation().t;
+
+  function handleContextMenu() {
+    return (
+      <ContextMenu
+        Opener={
+          <Tag variant="img" img_url="https://source.unsplash.com/featured/" />
+        }
+      >
+        <div className="flex flex-col gap-3">
+          <Menu.Item>
+            <Tag variant="txt" text={text("example")} />
+          </Menu.Item>
+          <Menu.Item>
+            <Tag variant="txt" text={text("example")} />
+          </Menu.Item>
+          <Menu.Item>
+            <Tag variant="txt" text={text("example")} />
+          </Menu.Item>
+          <Menu.Item>
+            <Tag variant="txt" text={text("example")} />
+          </Menu.Item>
+        </div>
+      </ContextMenu>
+    );
+  }
+
+  function handleTabBar() {
+    return [
+      <Tag
+        key={1}
+        variant="txt"
+        text={text("creatorpage:general")}
+        onClick={() => console.log("general")}
+      />,
+      <Tag
+        key={2}
+        variant="txt"
+        text={text("creatorpage:new")}
+        onClick={() => console.log("new")}
+      />,
+      <Tag
+        key={3}
+        variant="icn"
+        icon={ArrowsCounterClockwise}
+        onClick={() => console.log("switch")}
+      />,
+    ];
+  }
+
+  function handleMainTag() {
+    return (
+      <Tag
+        variant="img-txt"
+        text="example"
+        img_url="https://source.unsplash.com/featured/"
+      />
+    );
+  }
+
+  return (
+    <>
+      <Header
+        reightContent={handleContextMenu()}
+        background_url={props.headerImageUrl}
+      >
+        {handleMainTag()}
+      </Header>
+      <CreatorPageContent />
+      <TabBar isHidden={false} tags={handleTabBar()} />
+    </>
+  );
+}
---
+++ compositions/main/CreatorPage/CreatorPageContent.tsx
@@ -0,0 +1,59 @@
+import { Shortcut } from "../../../components/Shortcut/Shortcut";
+import { ShortcutGrid } from "../../../components/ShortcutGrid/ShortcutGrid";
+
+export function CreatorPageContent() {
+  function loadTemplates() {
+    return [
+      <Shortcut
+        key={1}
+        img_url="https://source.unsplash.com/featured/"
+        size="small"
+        title="template 1"
+      />,
+      <Shortcut
+        key={2}
+        img_url="https://source.unsplash.com/featured/"
+        size="small"
+        title="template 2"
+      />,
+      <Shortcut
+        key={3}
+        img_url="https://source.unsplash.com/featured/"
+        size="small"
+        title="template 3"
+      />,
+      <Shortcut
+        key={4}
+        img_url="https://source.unsplash.com/featured/"
+        size="large"
+        title="template 4"
+      />,
+      <Shortcut
+        key={5}
+        img_url="https://source.unsplash.com/featured/"
+        size="small"
+        title="template 5"
+      />,
+      <Shortcut
+        key={6}
+        img_url="https://source.unsplash.com/featured/"
+        size="large"
+        title="template 6"
+      />,
+    ];
+  }
+
+  return (
+    <div className="w-full h-screen bg-slate-100">
+      <div
+        className="fixed z-20 bottom-0 left-0 right-0 top-[76px] max-w-[1024px] mx-auto
+      rounded-t-[25px] overflow-y-scroll scrollbar-hide pt-2 px-2 bg-slate-100
+      md:pt-4 md:px-4 lg:z-0 lg:rounded-none lg:top-[148px] lg:pt-[5rem]"
+      >
+        <ShortcutGrid onDrag={(e) => console.log(e)}>
+          {loadTemplates()}
+        </ShortcutGrid>
+      </div>
+    </div>
+  );
+}
