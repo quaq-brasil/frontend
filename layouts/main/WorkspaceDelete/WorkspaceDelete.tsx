569e4929ca143cc38b8521d5af4103e12391b243
felpst <felipe.t.v.d@gmail.com>
2023-01-17 15:49:16 -0400

    feat(component): WorkspaceDelete

---
+++ layouts/main/WorkspaceDelete/WorkspaceDelete.tsx
@@ -0,0 +1,40 @@
+import useTranslation from "next-translate/useTranslation";
+import { Header } from "../../../components/Header/Header";
+import { TabBar } from "../../../components/TabBar/TabBar";
+import { Tag } from "../../../components/Tag/Tag";
+import { WorkspaceDeleteContent } from "./WorkspaceDeleteContent";
+
+export default function WorkspaceDelete() {
+  const text = useTranslation().t;
+
+  function handleTabBar() {
+    return [
+      <Tag
+        key={1}
+        variant="txt"
+        text={text("wsdelete:tab1")}
+        onClick={() => console.log("tab1")}
+      />,
+    ];
+  }
+
+  return (
+    <div className="bg-slate-100 fixed inset-0">
+      <Header
+        background_url={
+          "https://images.unsplash.com/photo-1464802686167-b939a6910659?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1700&q=80"
+        }
+      >
+        <Tag
+          variant="img-txt"
+          text={text("wsdelete:titletag")}
+          img_url="https://source.unsplash.com/featured/"
+        />
+        <Tag variant="txt" text={text("wsdelete:titletag2")} />
+        <Tag variant="txt" text={text("wsdelete:titletag3")} />
+      </Header>
+      <WorkspaceDeleteContent />
+      <TabBar isHidden={false} tags={handleTabBar()} />
+    </div>
+  );
+}
