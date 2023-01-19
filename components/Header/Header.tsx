4cd48f720b14e51129d1c7452d84b9bcd8125692
felpst <felipe.t.v.d@gmail.com>
2023-01-18 20:47:42 -0400

    fix(component): :art: Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -14,12 +14,16 @@ export const Header = ({
   return (
     <header
       className={`flex fixed z-10 top-0 left-0 right-0 bg-image
-      justify-between min-h-[6.875rem] px-4 pb-[2rem] max-w-[1024px]
+      justify-between min-h-[6.875rem] pb-[2rem] max-w-[1024px]
       lg:rounded-[2.5rem] mx-auto lg:min-h-[2.5rem] lg:px-10 
       lg:py-10 text-white lg:mt-[1.5rem]`}
     >
-      <div className="z-10 flex space-x-3">{children}</div>
-      <div className="z-10 flex space-x-3">{reightContent}</div>
+      <div className="pl-4 z-10 flex gap-3 overflow-x-auto scrollbar-hide">
+        {children}
+      </div>
+      {reightContent && (
+        <div className="pr-4 z-10 flex space-x-3">{reightContent}</div>
+      )}
 
       <Image
         src={background_url}
