28ead642b73074d7450ae1bdcb68d3238e6efbdc
felpst <felipe.t.v.d@gmail.com>
2023-01-20 10:15:13 -0400

    feat(component): AutomationBlock

---
+++ components/AutomationBlock/AutomationBlock.tsx
@@ -0,0 +1,71 @@
+import useTranslation from "next-translate/useTranslation"
+import { EyeSlash, PencilSimple } from "phosphor-react"
+import { useState } from "react"
+import { Button } from "../Button/Button"
+import { AutomationBlockBlocks } from "./AutomationBlockBlocks"
+
+type Spec = {
+  title: string
+  description: string
+}
+
+type AutomationBlockProps = {
+  description: string
+  specs: Spec[]
+  isEditable: boolean
+  isVisible: boolean
+}
+
+export const AutomationBlock = (props: AutomationBlockProps) => {
+  const text = useTranslation().t
+
+  const [blocksOpen, setBlocksOpen] = useState<boolean>(false)
+
+  function handleOpenBlocks() {
+    setBlocksOpen(!blocksOpen)
+  }
+
+  return (
+    <div
+      className={`flex relative flex-col gap-[0.3125rem] justify-center
+            min-w-[100%] bg-white p-[0.75rem] rounded-[20px] lg:rounded-[30px]
+            ${
+              props.isVisible === false &&
+              props.isEditable === false &&
+              "hidden"
+            }
+            `}
+    >
+      {props.isEditable && (
+        <div className="z-10 absolute shrink-0 flex content-center rounded-full bg-white right-0 top-0">
+          <PencilSimple className="w-[1rem] h-[1rem] m-[5px] lg:w-[1.25rem] lg:h-[1.25rem] drop-shadow-md" />
+        </div>
+      )}
+      <div className="flex flex-row gap-3 items-center  mb-[0.5rem]">
+        {!props.isVisible && (
+          <div className="w-[1.25rem] h-[1.25rem] lg:w-[1.75rem] lg:h-[1.75rem]">
+            <EyeSlash className="w-[1.25rem] h-[1.25rem] lg:w-[1.75rem] lg:h-[1.75rem]" />
+          </div>
+        )}
+        <p className="lg:text-[1.1rem] inline-block w-auto">
+          {props.description}
+        </p>
+      </div>
+      <span className="w-full p-[0.5px] bg-slate-100 mb-[0.5rem]"></span>
+      {props.specs.map((spec) => (
+        <div className="mb-[0.5rem]" key={spec.description}>
+          <p className="lg:text-[1.1rem] font-semibold">{spec.title}</p>
+          <p className="lg:text-[1.1rem]">{spec.description}</p>
+        </div>
+      ))}
+      <div className="w-full pt-3">
+        <Button
+          color="slate-900"
+          onClick={handleOpenBlocks}
+          text={text("automationblock:button")}
+        />
+      </div>
+      <AutomationBlockBlocks isOpen={blocksOpen} setIsOpen={handleOpenBlocks} />
+    </div>
+  )
+}
---
+++ components/AutomationBlock/AutomationBlockBlocks.tsx
@@ -0,0 +1,45 @@
+import useTranslation from "next-translate/useTranslation"
+import { BlockSelector } from "../BlockSelector/BlockSelector"
+import { Dialog } from "../Dialog/Dialog"
+import { TabBar } from "../TabBar/TabBar"
+import { Tag } from "../Tag/Tag"
+
+type AutomationBlockBlocksProps = {
+  isOpen: boolean
+  setIsOpen: () => void
+  size?: "sm" | "md" | "full"
+}
+
+export function AutomationBlockBlocks(props: AutomationBlockBlocksProps) {
+  const text = useTranslation().t
+
+  function handleTabBar() {
+    return [
+      <Tag
+        key={1}
+        variant="txt"
+        text={text("automationblock:back")}
+        onClick={() => props.setIsOpen()}
+      />,
+    ]
+  }
+
+  return (
+    <>
+      <Dialog
+        height={props.size}
+        isOpen={props.isOpen}
+        title={text("automationblock:toptitle")}
+        onClose={() => console.log("closed")}
+      >
+        <div className="flex flex-col items-center gap-3 lg:gap-6">
+          <BlockSelector onBlockSelect={() => console.log()} />
+          <TabBar
+            isHidden={props.size === "sm" ? true : false}
+            tags={handleTabBar()}
+          />
+        </div>
+      </Dialog>
+    </>
+  )
+}
