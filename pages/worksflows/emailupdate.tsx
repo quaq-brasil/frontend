0f944b256eb5aaeb245d2db4e8d8f6b86d5499b9
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2023-01-26 09:53:14 -0300

    feat: add services email update page

--- pages/worksflows/emailupdate.tsx
+++ pages/worksflows/emailupdate.tsx
@@ -1,8 +1,8 @@
-import EmailUpdate from "../layouts/Workflows/EmailUpdate/EmailUpdate"
-import { useUpdateUser } from "../services/hooks/useUser/useUpdateUser"
-import { useUser } from "../services/hooks/useUser/useUser"
+import EmailUpdate from "../../layouts/Workflows/EmailUpdate/EmailUpdate"
+import { useUpdateUser } from "../../services/hooks/useUser/useUpdateUser"
+import { useUser } from "../../services/hooks/useUser/useUser"
 
-export default function Home() {
+export default function EmailUpdatePage() {
   const response = useUser({
     id: "63d26f06ea1e68c873e97ab0",
   })
