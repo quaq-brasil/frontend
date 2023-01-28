07acc39462041df0a16d57ce44419a889fcfa235
felpst <felipe.t.v.d@gmail.com>
2023-01-28 12:00:26 -0300

    update centraltackers page

--- layouts/main/CentralTrackers/CentralTrackers.tsx
+++ layouts/main/CentralTrackers/CentralTrackers.tsx
@@ -1,20 +1,16 @@
 import useTranslation from "next-translate/useTranslation"
+import { useEffect, useState } from "react"
 import { Header } from "../../../components/Header/Header"
 import { TabBar } from "../../../components/TabBar/TabBar"
 import { Tag } from "../../../components/Tag/Tag"
+import { IPage, IUpdatePage } from "../../../types/Page.type"
+import { ITemplate, IUpateTemplate } from "../../../types/Template.type"
 import { CentralTrackersContent } from "./CentralTrackersContent"
 
-type IData = {
-  name: string
-  avatar_url?: string
-  background_url?: string
-  shortcut_image?: string
-}
-
 type CentralTrackersProps = {
-  handleUpdateTrackers: (face?: string, google?: string) => void
-  pageData?: IData
-  templateData?: IData
+  handleUpdateTrackers: (data: IUpateTemplate) => void
+  pageData?: IPage
+  templateData?: ITemplate
 }
 
 export default function CentralTrackers({
@@ -24,43 +20,77 @@ export default function CentralTrackers({
 }: CentralTrackersProps) {
   const text = useTranslation().t
 
+  const [isUpdating, setIsUpdating] = useState<boolean>(false)
+  const [page, setPage] = useState<IUpdatePage>()
+  const [template, setTemplate] = useState<IUpateTemplate>()
+
+  function handleUpdateIsUpdating(stat: boolean) {
+    setIsUpdating(stat)
+  }
+
+  function handleUpdatePage(page: IUpdatePage) {
+    setPage(page)
+  }
+
+  function handleUpdateTemplate(template: IUpateTemplate) {
+    setTemplate(template)
+  }
+
+  useEffect(() => {
+    handleUpdatePage(pageData as IUpdatePage)
+  }, [pageData])
+
+  useEffect(() => {
+    handleUpdateTemplate(templateData as IUpateTemplate)
+  }, [templateData])
+
+  const [runUpdate, setRunUpdate] = useState(false)
+
+  function handleUpdateRunUpdate(stat: boolean) {
+    setRunUpdate(stat)
+  }
+
   function handleTabBar() {
-    return [
-      <Tag
-        key={1}
-        variant="txt"
-        text={text("centraltrackers:back")}
-        onClick={() => console.log()}
-      />,
-    ]
+    if (isUpdating) {
+      return [
+        <Tag
+          key={1}
+          variant="txt"
+          text={text("centraltrackers:back")}
+          onClick={() => console.log()}
+        />,
+        <div key={2} className="w-fit h-fit xl:hidden">
+          <Tag
+            variant="txt"
+            text={text("centraltrackers:update")}
+            onClick={() => handleUpdateRunUpdate(true)}
+          />
+        </div>,
+      ]
+    } else {
+      return [
+        <Tag
+          key={1}
+          variant="txt"
+          text={text("centraltrackers:back")}
+          onClick={() => console.log()}
+        />,
+      ]
+    }
   }
 
   function loadHeader() {
     return (
-      <Header
-        background_url={
-          pageData?.background_url
-            ? pageData.background_url
-            : "https://images.unsplash.com/photo-1464802686167-b939a6910659?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1700&q=80"
-        }
-      >
+      <Header background_url={page?.background_url || ""}>
         <Tag
           variant="img-txt"
-          text={pageData?.name ? pageData.name : "page title"}
-          img_url={
-            pageData?.avatar_url
-              ? pageData.avatar_url
-              : "https://images.unsplash.com/photo-1464802686167-b939a6910659?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1700&q=80"
-          }
+          text={page?.name || ""}
+          img_url={page?.avatar_url || ""}
         />
         <Tag
           variant="img-txt"
-          text={templateData?.name ? templateData.name : "page title"}
-          img_url={
-            templateData?.shortcut_image
-              ? templateData.shortcut_image
-              : "https://images.unsplash.com/photo-1464802686167-b939a6910659?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1700&q=80"
-          }
+          text={template?.name || ""}
+          img_url={template?.shortcut_image || ""}
         />
         <Tag variant="txt" text={text("centraltrackers:trackers")} />
       </Header>
@@ -70,7 +100,14 @@ export default function CentralTrackers({
   return (
     <div className="bg-slate-100 fixed inset-0">
       {loadHeader()}
-      <CentralTrackersContent handleUpdateTrackers={handleUpdateTrackers} />
+      <CentralTrackersContent
+        handleUpdateTrackers={handleUpdateTrackers}
+        isUpdating={isUpdating}
+        handleUpdateIsUpdating={handleUpdateIsUpdating}
+        templateData={template}
+        runUpdate={runUpdate}
+        handleUpdateRunUpdate={handleUpdateRunUpdate}
+      />
       <TabBar isHidden={false} tags={handleTabBar()} />
     </div>
   )
