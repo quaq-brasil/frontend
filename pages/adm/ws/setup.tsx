44f469d2bdb213e750335be1fd9e084c67a9237e
felpst <felipe.t.v.d@gmail.com>
2023-02-07 18:02:06 -0300

    refactor(pages structure): :fire: delete setup, update first-workspace to setup

--- pages/adm/ws/setup.tsx
+++ pages/adm/ws/setup.tsx
@@ -1,29 +1,5 @@
-import { useUserAuth } from "../../../contexts/userAuth"
-import WorkspaceSetup from "../../../layouts/Onboarding/WorkspaceSetup/WorkspaceSetup"
-import { useCreateWorkspace } from "../../../services/hooks/useWorkspace/useCreateWorkspace"
+import FirstWorkspace from "../../../layouts/Onboarding/FirstWorkspace/FirstWorkspace"
 
-export default function WorkspaceSetupPage() {
-  const { user } = useUserAuth()
-
-  const createWorkspace = useCreateWorkspace()
-
-  const handleCreateWorkspace = (name: string, avatar_url: string) => {
-    createWorkspace.mutate({
-      data: {
-        name: name,
-        user_id: "1",
-        avatar_url: avatar_url,
-        services: [
-          {
-            name: "free",
-            description: "free workspace",
-          },
-        ],
-      },
-    })
-  }
-
-  return (
-    <WorkspaceSetup data={user} handleCreateWorkspace={handleCreateWorkspace} />
-  )
+export default function FirstWorkspacePage() {
+  return <FirstWorkspace />
 }
