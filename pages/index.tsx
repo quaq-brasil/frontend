d2387fb205bd43d507704920eda8108c8977756d
felpst <felipe.t.v.d@gmail.com>
2023-02-28 15:21:57 -0300

    fix: :zap: add ssr to 404 and main index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,30 @@
+import { GetServerSideProps } from "next"
 import Explorer from "../layouts/main/Explorer/Explorer"
+import { api } from "../services/api"
 import { usePage } from "../services/hooks/usePage/usePage"
 import { useTemplatesByPageId } from "../services/hooks/useTemplate/useTemplatesByPageId"
 import { IPage } from "../types/Page.type"
 import { ITemplate } from "../types/Template.type"
+import { RedirectNotFoundVerify } from "../utils/404Redirect"
 
 type ConsumerPagePageProps = {
-  pageId: string
+  pageData: IPage
+  templateData: ITemplate
 }
 
-export default function Home({ pageId }: ConsumerPagePageProps) {
+export default function Home({
+  pageData,
+  templateData,
+}: ConsumerPagePageProps) {
   const getPage = usePage({
     id: "63b754987d02f98b8692255e",
+    options: { initialData: pageData },
   })
 
-  const getTemplates = useTemplatesByPageId({ id: "63b754987d02f98b8692255e" })
+  const getTemplates = useTemplatesByPageId({
+    id: "63b754987d02f98b8692255e",
+    options: { initialData: templateData },
+  })
 
   return (
     <Explorer
@@ -21,4 +32,21 @@ export default function Home({ pageId }: ConsumerPagePageProps) {
       initialTemplatesData={getTemplates?.data as ITemplate[]}
     />
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  async function getPage() {
+    const { data: pageData } = await api.get(`/pages/63b754987d02f98b8692255e`)
+
+    const { data: templateData } = await api.get(
+      `/templates/page/63b754987d02f98b8692255e`
+    )
+
+    return {
+      pageData: { pageData },
+      templateData: { templateData },
+    }
+  }
+
+  return await RedirectNotFoundVerify(getPage, ctx)
 }
