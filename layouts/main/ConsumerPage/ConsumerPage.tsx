dc423fdf85ec8a9f81a6702f5f07cf092c6b049c
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2023-03-15 09:53:19 -0300

    feat: deploy

--- layouts/main/ConsumerPage/ConsumerPage.tsx
+++ layouts/main/ConsumerPage/ConsumerPage.tsx
@@ -44,8 +44,7 @@ export default function ConsumerPage({
       if (!isLoggedIn) {
         return (
           <div
-            className={`flex fixed z-10 top-0 left-0 right-0 bg-image
-      justify-end min-h-[6.875rem] pb-[2rem] max-w-[1024px]
+            className={`flex fixed z-10 top-0 left-0 right-0 bg-image justify-end min-h-[6.875rem] pb-[2rem] max-w-[1024px]
       lg:rounded-[2.5rem] mx-auto lg:min-h-[2.5rem] lg:px-10 
       lg:py-10 text-white lg:mt-[1.5rem] mt-[19px] md:mt-0"`}
           >
