3c6a2a7273762faa254da0678e8bc32c5b27e38f
felpst <felipe.t.v.d@gmail.com>
2023-03-20 10:20:47 -0300

    fix(page): login

--- layouts/Onboarding/LogIn/LogIn.tsx
+++ layouts/Onboarding/LogIn/LogIn.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import { IUserLogin } from "types/User.type"
 import { pageUrls } from "utils/pagesUrl"
-import { LoginContent } from "./LogInContent"
+import { LoginContent } from "./LoginContent"
 
 type LoginProps = {
   handleUserLogin: (data: IUserLogin) => void
