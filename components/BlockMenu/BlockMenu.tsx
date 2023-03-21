074e25d4cd430dbd9474e5d764b07193be34e280
felpst <felipe.t.v.d@gmail.com>
2023-03-21 13:44:04 -0300

    fix(component): :art: add move block to block menu

--- components/BlockMenu/BlockMenu.tsx
+++ components/BlockMenu/BlockMenu.tsx
@@ -1,7 +1,17 @@
 import { Menu, MenuHandler, MenuItem, MenuList } from "@material-tailwind/react"
 import useTranslation from "next-translate/useTranslation"
-import { DotsThree } from "phosphor-react"
+import dynamic from "next/dynamic"
 import { useState } from "react"
+
+const ArrowsOutCardinal = dynamic(() =>
+  import("phosphor-react").then((mod) => mod.ArrowsOutCardinal)
+)
+
+const DotsThree = dynamic(() =>
+  import("phosphor-react").then((mod) => mod.DotsThree)
+)
+
+const X = dynamic(() => import("phosphor-react").then((mod) => mod.X))
 
 export type BlockMenuProps = {
   onEdit?: () => void
@@ -11,56 +21,82 @@ export type BlockMenuProps = {
 
 export function BlockMenu({ onDelete, onDrag, onEdit }: BlockMenuProps) {
   const text = useTranslation().t
+
   const [selected, setSelected] = useState(false)
+  const [isMoveSelected, setIsMoveSelected] = useState(false)
+
   return (
     <div className="absolute right-0 top-0 z-10">
       <Menu placement="left-start">
-        <button onClick={() => setSelected(!selected)} className="w-fit h-fit">
-          <MenuHandler>
+        {!isMoveSelected && (
+          <>
             <button
-              className={`h-[1.5rem] w-[1.5rem] rounded-full flex justify-center items-center ${
-                selected ? "bg-slate-900 text-white" : "bg-white text-black"
-              } ring-1 ring-slate-100`}
+              onClick={() => setSelected(!selected)}
+              className="w-fit h-fit"
             >
-              <DotsThree className="w-[2rem] h-[2rem]" weight="bold" />
+              <MenuHandler>
+                <button
+                  className={`h-[1.5rem] w-[1.5rem] rounded-full flex justify-center items-center ${
+                    selected ? "bg-slate-900 text-white" : "bg-white text-black"
+                  } ring-1 ring-slate-100`}
+                >
+                  <DotsThree className="w-[2rem] h-[2rem]" weight="bold" />
+                </button>
+              </MenuHandler>
             </button>
-          </MenuHandler>
-        </button>
-        <MenuList className="rounded-xl text-[1rem] lg:text-[1rem] px-0 py-0">
-          <MenuItem className="flex justify-start w-full">
-            <p
-              onClick={() => {
-                setSelected(false)
-                onEdit && onEdit()
-              }}
-              className="flex flex-row gap-2 justify-center items-center py-1 px-2 mt-2 rounded-xl hover:bg-slate-100"
-            >
-              {text("blockmenu:edit")}
-            </p>
-          </MenuItem>
-          <MenuItem className="flex justify-start w-full">
-            <p
-              onClick={() => {
-                setSelected(false)
-                onDrag && onDrag()
-              }}
-              className="flex flex-row gap-2 justify-center items-center py-1 px-2 rounded-xl hover:bg-slate-100"
-            >
-              {text("blockmenu:drag")}
-            </p>
-          </MenuItem>
-          <MenuItem className="flex justify-start w-full">
-            <p
-              onClick={() => {
-                setSelected(false)
-                onDelete && onDelete()
-              }}
-              className="flex flex-row gap-2 justify-center items-center py-1 px-2 rounded-xl hover:bg-slate-100"
-            >
-              {text("blockmenu:delete")}
-            </p>
-          </MenuItem>
-        </MenuList>
+            <MenuList className="rounded-xl text-[1rem] lg:text-[1rem] px-0 py-0">
+              <MenuItem className="flex justify-start w-full">
+                <p
+                  onClick={() => {
+                    setSelected(false)
+                    onEdit && onEdit()
+                  }}
+                  className="flex flex-row gap-2 justify-center items-center py-1 px-2 mt-2 rounded-xl hover:bg-slate-100"
+                >
+                  {text("blockmenu:edit")}
+                </p>
+              </MenuItem>
+              <MenuItem className="flex justify-start w-full">
+                <p
+                  onClick={() => {
+                    setIsMoveSelected(true)
+                  }}
+                  className="flex flex-row gap-2 justify-center items-center py-1 px-2 rounded-xl hover:bg-slate-100"
+                >
+                  {text("blockmenu:drag")}
+                </p>
+              </MenuItem>
+              <MenuItem className="flex justify-start w-full">
+                <p
+                  onClick={() => {
+                    setSelected(false)
+                    onDelete && onDelete()
+                  }}
+                  className="flex flex-row gap-2 justify-center items-center py-1 px-2 rounded-xl hover:bg-slate-100"
+                >
+                  {text("blockmenu:delete")}
+                </p>
+              </MenuItem>
+            </MenuList>
+          </>
+        )}
+        {isMoveSelected && (
+          <>
+            <button onClick={() => {}} className="w-fit h-fit">
+              <button
+                className={`h-[1.5rem] w-[1.5rem] rounded-full flex justify-center items-center bg-slate-900 text-white ring-1 ring-slate-100`}
+              >
+                <ArrowsOutCardinal className="w-[1.15rem] h-[1.15rem] ml-[1px]" />
+              </button>
+              <button
+                onClick={() => setIsMoveSelected(false)}
+                className={`h-[1.5rem] w-[1.5rem] rounded-full flex justify-center items-center mt-2 bg-white text-black ring-1 ring-slate-100`}
+              >
+                <X className="w-[1.15rem] h-[1.15rem] ml-[1px]" />
+              </button>
+            </button>
+          </>
+        )}
       </Menu>
     </div>
   )
