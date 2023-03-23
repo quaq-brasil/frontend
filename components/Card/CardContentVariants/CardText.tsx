727314e671723e96c2d4aad0778a7426c147d8e0
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2023-03-23 09:31:47 -0300

    feat: improve components

--- components/Card/CardContentVariants/CardText.tsx
+++ components/Card/CardContentVariants/CardText.tsx
@@ -12,46 +12,48 @@ type CardTextProps = {
   onClick?: () => void
 }
 
-export function CardText(props: CardTextProps) {
-  function handleClick() {
-    if (props?.onClick) {
-      props.onClick()
-    }
-  }
+type IndicatorProps = {
+  icon?: ForwardRefExoticComponent<IconProps & RefAttributes<SVGSVGElement>>
+  text?: string
+  isVisible?: boolean
+  onClick?: () => void
+}
 
+const Indicator: React.FC<IndicatorProps> = ({
+  icon: Icon,
+  text,
+  isVisible,
+  onClick,
+}) => (
+  <>
+    {Icon && (
+      <Icon
+        onClick={onClick}
+        className={`w-[1.375rem] h-[1.375rem] m-[0.3125rem] lg:w-[1.5625rem] lg:h-[1.5625rem] ${
+          isVisible ? "text-white" : ""
+        }`}
+      />
+    )}
+    {text && (
+      <p
+        className={`lg:text-[1.1rem] text-right m-[0.3125rem] line-clamp-2 ${
+          isVisible ? "text-white" : ""
+        }`}
+      >
+        {text}
+      </p>
+    )}
+  </>
+)
+
+export function CardText({ label, indicator, onClick }: CardTextProps) {
   return (
     <button
       className="flex flex-row justify-between items-center px-3 lg:px-[1.125rem] min-w-[100%]"
-      onClick={() => handleClick()}
+      onClick={onClick}
     >
-      {props.label && (
-        <p className="lg:text-[1.1rem] text-left">{props.label}</p>
-      )}
-      {props.indicator && (
-        <>
-          {props.indicator.icon &&
-            (props.indicator.onClick ? (
-              <props.indicator.icon
-                onClick={props.indicator.onClick}
-                className={`w-[1.375rem] h-[1.375rem] m-[0.3125rem] lg:w-[1.5625rem]
-             lg:h-[1.5625rem] ${props.indicator.isVisible ? "text-white" : ""}`}
-              />
-            ) : (
-              <props.indicator.icon
-                className={`w-[1.375rem] h-[1.375rem] m-[0.3125rem] lg:w-[1.5625rem]
-             lg:h-[1.5625rem] ${props.indicator.isVisible ? "text-white" : ""}`}
-              />
-            ))}
-          {props.indicator.text && (
-            <p
-              className={`lg:text-[1.1rem] text-right m-[0.3125rem] line-clamp-2
-            ${props.indicator.isVisible ? "text-white" : ""}`}
-            >
-              {props.indicator.text}
-            </p>
-          )}
-        </>
-      )}
+      {label && <p className="lg:text-[1.1rem] text-left">{label}</p>}
+      {indicator && <Indicator {...indicator} />}
     </button>
   )
 }
