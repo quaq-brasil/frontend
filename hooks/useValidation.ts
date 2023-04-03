8d118e00fdb46639728fc5db1c33323c3b48efe2
felpst <felipe.t.v.d@gmail.com>
2023-04-03 16:30:38 -0300

    fix(block config): add new useValidation to embed config

--- hooks/useValidation.ts
+++ hooks/useValidation.ts
@@ -97,6 +97,8 @@ function createValidator<T>(rule: ValidationRule<T>): Validator<T> {
 const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 const strongPasswordPattern =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/
+const urlPattern =
+  /^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/
 
 export const validationRules = {
   required:
@@ -136,5 +138,10 @@ export const validationRules = {
     ): Validator<T> =>
     (value: T) => {
       return !customValidator(value, ...(args || [])) ? errorMessage : null
+    },
+  url:
+    (errorMessage: string): Validator<string> =>
+    (value: string) => {
+      return !urlPattern.test(value) ? errorMessage : null
     },
 }
