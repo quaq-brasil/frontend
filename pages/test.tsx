ebff1baa0f4ccc09f0fbef6940e020ff6fb71c65
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2023-04-20 20:02:06 -0300

    test

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,11 +1,22 @@
 export default function Page() {
   return (
     <div>
-      <embed
-        src="https://quaq-image.s3.sa-east-1.amazonaws.com/books/Algorithms.pdf"
-        type="application/pdf"
-        className="w-full h-screen"
-      />
+      <div>
+        <object
+          data="https://quaq-image.s3.sa-east-1.amazonaws.com/books/Algorithms.pdf"
+          type="application/pdf"
+          className="w-full h-screen"
+        >
+          <p>
+            Your browser does not support PDFs. Please download the PDF to view
+            it:{" "}
+            <a href="https://quaq-image.s3.sa-east-1.amazonaws.com/books/Algorithms.pdf">
+              Download PDF
+            </a>
+            .
+          </p>
+        </object>
+      </div>
     </div>
   )
 }
